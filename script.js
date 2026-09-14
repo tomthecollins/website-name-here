a969d77de17d72ec2231cdf047f4f8a23443628a
Tom Collins <tomthecollins@gmail.com>
2026-09-13 22:52:17 -0400

    Fixes galore!

--- script.js
+++ script.js
@@ -8,17 +8,14 @@ This is your site JavaScript code - you can add interactivity and carry out proc
 console.log("Hello 🌎");
 
 /*
-Make the "Click me!" button move when the visitor clicks it:
+Make the "Click me!" move when the visitor clicks it:
 - First add the button to the page by following the "Next steps" in the README
 */
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
-if (btn) {
-  btn.onclick = function() {
-    console.log("GOT HERE!")
-    // The JS works in conjunction with the 'dipped' code in style.css
-    btn.classList.toggle("dipped");
-  };
+const image = document.querySelector(".illustration")
+
+image.onclick = function() {
+  console.log("GOT HERE!")
+  image.classList.toggle("dipped")
 }
 
 // This is a single line JS comment
